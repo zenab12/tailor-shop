b686a59b3579aa0db779ad869682663f032d932b
zenab mohammed abdelmonaem <78083890+zenab12@users.noreply.github.com>
2021-03-26 01:10:01 +0200

    father shop arabic site with bootstrap3

---
+++ js/plugin.js
@@ -0,0 +1,15 @@
+/*global $*/
+$(document).ready(function() {
+    "use strict";
+    /*  let marginleft = $(".services h2").width() - 50;
+      let marginright = $(".services h2").width() - 50;
+      let width = $('.services h2').width() - 50;
+      let borderBefore = $('.border-before');
+      let borderAfter = $('.border-after');
+
+      borderBefore.css('margin-left', marginleft);
+      borderAfter.css('margin-right', marginright);
+      borderAfter.css('width', width);
+      borderAfter.css('width', width);
+      */
+});

18780ad3b1c6d75a187a7991a3d2b1d674719bfe
zenab mohammed abdelmonaem <78083890+zenab12@users.noreply.github.com>
2021-04-07 17:39:51 +0200

    add new pages

--- js/plugin.js
+++ js/plugin.js
@@ -12,4 +12,66 @@ $(document).ready(function() {
       borderAfter.css('width', width);
       borderAfter.css('width', width);
       */
+
+
+    $('.carousel').carousel({ interval: 3000 });
+
+    // show option box
+    $('.fa-palette').click(function() {
+
+        $('.coloroption').toggle();
+    });
+
+    // change themes colors 
+
+    $('.coloroption ul li').eq(0).css("backgroundColor", "#f0ad4e");
+    $('.coloroption ul li').eq(1).css("backgroundColor", "#5cb85c");
+    $('.coloroption ul li').eq(2).css("backgroundColor", "#5bc0de");
+    $('.coloroption ul li').eq(3).css("backgroundColor", "#d9534f");
+
+    var colorLi = $('.coloroption ul li');
+
+    colorLi.click(function() {
+
+        $('link[href*="heme"]').attr('href', $(this).attr('data-value'));
+    });
+
+
+
+    //scroll button
+    var scrollButton = $(".scroll-to-top");
+
+    $(window).scroll(function() {
+
+        if ($(this).scrollTop() >= 700) {
+            scrollButton.fadeIn(500);
+        } else {
+            scrollButton.fadeOut(500);
+        }
+    });
+
+    scrollButton.click(function() {
+        $('html,body').animate({ scrollTop: 0 }, 2000);
+    });
+
+
+
+    //loading screen
+
+    $(window).on('load', function() {
+
+        $('body').css({ "overflow": "auto" });
+
+        $('.scissors').fadeOut(200, function() {
+
+            $(this).parent().fadeOut(300, function() {
+                $(this).remove()
+            })
+
+
+        })
+    });
+
+
+
 });
